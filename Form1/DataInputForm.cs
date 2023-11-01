52c128ecfc09a5176269bd3f546e28fead6c9b34
Gix <gibram.g.f@gmail.com>
2023-10-31 23:56:49 -0300

    primeira versao

---
+++ Form1/DataInputForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class DataInputForm : Form
+    {
+        public DataInputForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+    }
+}
--- Form1/Main.cs
+++ Form1/Main.cs
@@ -1,58 +1,29 @@
-﻿using InvenManager.Svc;
-
 namespace Form1
 {
-    public partial class Form1: Form
+    public partial class Main : Form
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public Main()
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            treeView1 = new TreeView();
-            SuspendLayout();
-            // 
-            // treeView1
-            // 
-            treeView1.Location = new Point(0, 0);
-            treeView1.Name = "treeView1";
-            treeView1.Size = new Size(191, 450);
-            treeView1.TabIndex = 0;
-            treeView1.AfterSelect += treeView1_AfterSelect;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(treeView1);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
+            InitializeComponent();
             PopulateRootNodes();
         }
-        #endregion
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView1_MouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Text == "Adicionar clientes")
+            {
+                DataInputForm dataInputForm = new DataInputForm();
+                dataInputForm.ShowDialog();
+            }
+        }
+
+
+
         private void PopulateRootNodes()
         {
             PopulateClienteNode();
@@ -124,25 +95,6 @@ namespace Form1
 
             treeView1.Nodes.Add(rootCliente);
         }
-
-        private void verClienteNode_DoubleClick(object sender, EventArgs e)
-        {
-            // Get the DataGridView control.
-            
-            DataGridView datagridCli = this.Controls["datagridCliente"] as DataGridView;
-            // Set the Visible property of the DataGridView control to true.
-            datagridCli.Visible = true;
-
-            // Bind the DataGridView control to a data source that contains the customer information.
-            datagridCli.DataSource = ClienteSvc.getBulkCliente();
-        }
-
-
-
-        private TreeView treeView1;
-        DataGridView gridCli = new DataGridView();
-       
-       
 
     }
 }
